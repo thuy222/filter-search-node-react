83476e00076263ff833ace53e5d48a88358c462d
thuy <thuy.ho@sunteco.io>
2022-10-23 17:15:16 +0700

    add api

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import { Users } from "./users.js";
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const { q } = req.query;
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(q))
+    );
+  };
+
+  q ? res.json(search(Users).slice(0, 10)) : res.json(Users.slice(0, 10));
+});
+
+app.listen(5000, () => console.log("API is working!"));
